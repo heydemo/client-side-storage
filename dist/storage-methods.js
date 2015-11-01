6deed7110891c346902ce02549fbb127ff5619b3
John De Mott <jdemott@opentactics.com>
2015-10-31 20:00:41 -0500

    chore(build): Updating Travis config

---
+++ dist/storage-methods.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var StorageAPI = require('./StorageAPI');
+
+StorageAPI.registerStorageMethod('localStorage', require('./Storage.localStorage'));
+//StorageAPI.registerStorageMethod('phonegap', require('./Storage.phonegap'));
+
+module.exports = StorageAPI;
