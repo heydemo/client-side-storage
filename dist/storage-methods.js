662834084a320ec7530a393aa655174451feb9f7
John De Mott <jdemott@opentactics.com>
2015-10-31 22:17:11 -0500

    fix(build): Removing dist directory

--- dist/storage-methods.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var StorageAPI = require('./StorageAPI');
-
-StorageAPI.registerStorageMethod('localStorage', require('./Storage.localStorage'));
-//StorageAPI.registerStorageMethod('phonegap', require('./Storage.phonegap'));
-
-module.exports = StorageAPI;
